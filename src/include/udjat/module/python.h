9449b3bfa3a0490a326af79cfc5e105ab2341904
Perry Werneck <perry.werneck@gmail.com>
2026-04-22 21:45:47 -0300

    Starting project.

---
+++ src/include/udjat/module/python.h
@@ -0,0 +1,48 @@
+/* SPDX-License-Identifier: LGPL-3.0-or-later */
+
+/*
+ * Copyright (C) 2023 Perry Werneck <perry.werneck@gmail.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+ #pragma once
+
+ #include <config.h>
+ #include <udjat/defs.h>
+ #include <udjat/module/abstract.h>
+ 
+ // #include <udjat/tools/actions/python.h>
+ // #include <udjat/agent/python.h>
+ // #include <udjat/tools/url.h>
+
+
+ namespace Udjat {
+
+	namespace Python {
+
+		/// @brief Generic http module.
+		class UDJAT_API Module : public Udjat::Module {
+		public:
+
+			static Udjat::Module * Factory(const char *name = "python");
+
+			Module(const char *name = "python", const char *description = nullptr);
+			~Module() override;
+
+		};
+
+	}
+
+ }
---
+++ src/library/module.cc
@@ -0,0 +1,43 @@
+/* SPDX-License-Identifier: LGPL-3.0-or-later */
+
+/*
+ * Copyright (C) 2024 Perry Werneck <perry.werneck@gmail.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+ #include <config.h>
+
+ #include <udjat/defs.h>
+ #include <udjat/module/abstract.h>
+ #include <udjat/tools/logger.h>
+ #include <udjat/tools/url.h>
+ #include <udjat/tools/url/handler/http.h>
+ #include <udjat/module/python.h>
+
+ namespace Udjat {
+
+	Udjat::Module * Python::Module::Factory(const char *name) {
+		return new Python::Module(name,"Python module");
+	}
+
+	Python::Module::Module(const char *name, const char *description) 
+		: Udjat::Module(name,(description ? description : "Python module")) {
+	}
+
+	Python::Module::~Module() {
+	}
+
+ }
+
